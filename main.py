7e531f50c6570073104dc09ca2d2b60d810e8e2e
sneakyPeat <d.ducluzeaud@gmail.com>
2017-09-27 14:10:12 +0200

    function place_random_object can now add a desired number of object

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from map import *
+from map import Maze
+from McGyver import McGyver
 
 """Open and read the file containing the maze. Each char in the file will get
    coordinates with a dictionnary
 """
-
-maze = Maze()
+mac = McGyver()
+maze = Maze(mac)
 maze.generate_maze()
 maze.display_maze()
-maze.place_random_object()
+
+maze.place_random_object(3)
 maze.display_maze()
--- map.py
+++ map.py
@@ -1,10 +1,14 @@
 import random
 
+from McGyver import McGyver
+
 class Maze:
 
-    def __init__(self):
+    def __init__(self, McGyver):
         self.file = "map.txt"
         self.coord = {}
+        self.McGyver = McGyver
+        self.coord[McGyver.x, McGyver.y] = McGyver # McGyver's position
 
     def generate_maze(self):
         """Open and read the file containing the maze. Each char in the file will get
@@ -21,15 +25,33 @@ class Maze:
 
     def display_maze(self):
         for value in self.coord.values():
-            print(value, end='')
+            print(value, end="")
 
-    def place_random_object(self):
-        object_placed = False
+    def place_random_object(self, n):
+        object_placed = 0
+        nb_objet = n
         random_object = '@'
 
-        while not object_placed:
+        while object_placed != nb_objet:
             x_random = random.randint(0, 14)
             y_random = random.randint(0, 14)
             if self.coord[(x_random, y_random)] == " ":
                 self.coord[(x_random, y_random)] = random_object
-                object_placed = True
+                object_placed += 1
+            
+
+    def teleport(self, McGyver):
+        """McGyver has the ability to go anywhere in the map"""
+        teleport = False
+
+        while not teleport:
+            line = input()
+            column = input()
+            if self.coord[(line, column)] == " ":
+                # delete the position of McGyver
+                del self.coord[(McGyver.x, McGyver.y)]
+                # McGyver teleport himself in the new position
+                self.coord[line, column] = McGyver
+                teleport = True
+            else:
+                print("McGyver can't teleport here, try again !")
